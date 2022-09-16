a16286507485d0fbe3ee8075ebcc7931095656b9
Amir Sajad Nemati <arsad@Amirs-MacBook-Pro.local>
2022-09-16 08:01:37 -0700

    Set Up PlayListViewController And It's UI

---
+++ Spotify/Spotify/ViewModels/PlayListHeaderViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlayListHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by Amir Sajad Nemati on 9/16/22.
+//
+
+import Foundation
---
+++ Spotify/Spotify/Views/PlayListHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  PlayListHeaderCollectionReusableView.swift
+//  Spotify
+//
+//  Created by Amir Sajad Nemati on 9/16/22.
+//
+
+import UIKit
+
+class PlayListHeaderCollectionReusableView: UICollectionReusableView {
+        
+}
